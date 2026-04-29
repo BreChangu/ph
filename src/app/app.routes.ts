4f6a0bd64b308e77128388df8c8cf7e4b384b377
Fernando <ferchin911@gmail.com>
2026-04-29 17:01:37 -0600

    mejora en el home, creación de secciones acerca de y servicios, modificación en rutas, mejora de navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,15 @@ export const routes: Routes = [
         path: '', 
         // Asumiendo que home se generó siguiendo la misma regla limpia
         loadComponent: () => import('./features/home/home').then(m => m.Home)
+      },
+
+      {
+        path: 'servicios', // Nueva ruta (pabloherrera.com/servicios)
+        loadComponent: () => import('./features/servicios/servicios').then(m => m.Servicios)
+      },
+      {
+        path: 'acerca', // Nueva ruta (pabloherrera.com/acerca)
+        loadComponent: () => import('./features/acerca/acerca').then(m => m.Acerca)
       }
     ]
   },
--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Home } from '../../features/home/home';
+import { Servicios } from '../../features/servicios/servicios';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink,Home,Servicios],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
 
+  // Variable de estado para controlar el menú móvil
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    // Detalle de UX Premium: Bloquear el scroll del fondo cuando el menú está abierto
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+    document.body.style.overflow = '';
+  }
 }
+
