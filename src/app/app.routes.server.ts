1fd88451d813838352179c93536588b6d821c6de
Fernando <ferchin911@gmail.com>
2026-05-01 14:46:28 -0600

    fix en angular.json, en angular.routes.server.ts para que cargue las paginas del blog por separado

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,7 +1,14 @@
+// src/app/app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    // Le decimos que NO pre-renderice las notas del blog individualmente
+    path: 'blog/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    // Pero que el resto de la página (Home, Acerca, etc.) sí lo haga para que sea ultra rápida
     path: '**',
     renderMode: RenderMode.Prerender
   }
