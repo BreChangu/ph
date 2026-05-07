2a7d135efe3b8022716658d2362cd7622f9f163c
Fernando <ferchin911@gmail.com>
2026-05-06 20:12:27 -0600

    creación de las variables de entorno para poder usar supabase

---
+++ src/enviroments/enviroments.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false, // En el archivo environment.ts normal, esto puede ir en true
+  
+  // 1. La URL de tu proyecto (la que vimos en la primera captura)
+  supabaseUrl: 'https://xxilpdpdfxmxgesdurhp.supabase.co',
+  
+  // 2. La Publishable Key (la que empieza con sb_publishable_...)
+  supabaseKey: 'sb_publishable_JRElFQgwpH0-Q_rn2GUdXA_podcppB9'
+};
